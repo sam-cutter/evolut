33010ab58334f79a599130534bd86c2f9b26d48b
Sam Cutter <swcutter@pm.me>
2024-10-05 16:07:30 +0100

    Created the Brain struct, as well as figured out what needs to be done to implement its constuctor

--- src/brain/mod.rs
+++ src/brain/mod.rs
@@ -1,2 +1,29 @@
 pub mod connection;
 pub mod neuron;
+
+use connection::Connection;
+use neuron::Neuron;
+
+pub struct Brain<'a> {
+    connections: Vec<Connection<'a>>,
+    neurons: Vec<Neuron>,
+}
+
+// TODO: Implement constructor
+impl<'a> Brain<'a> {
+    pub fn new() {
+        // This function will have a genome passed to it by reference
+
+        /*
+        Step 1: creating required neurons.
+            For each specified connection in the genome, the source and destination neurons will be created.
+        Step 2: creating required connections.
+            Each connection can now be built with the newly created neurons.
+        Step 3: connection pruning.
+            The connections can now be pruned to ensure that no loops or cycles are introduced into the network.
+        Step 4: neuron pruning.
+            Any unused neurons can now be pruned.
+        Step 5: return all connections and neurons.
+        */
+    }
+}
