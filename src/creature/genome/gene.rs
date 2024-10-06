7f8f4191b6f12b639d3b547c61e9202581d35bd9
Sam Cutter <swcutter@pm.me>
2024-10-06 22:15:51 +0100

    Moved Gene into its own module

---
+++ src/creature/genome/gene.rs
@@ -0,0 +1,37 @@
+use fstr::FStr;
+use std::{num::ParseIntError, str::FromStr};
+
+pub struct Gene {
+    source_id: u8,
+    destination_id: u8,
+    weight: f64,
+}
+
+impl Gene {
+    pub fn from_hex(hex: FStr<20>) -> Result<Self, ParseIntError> {
+        let source_id = &hex[0..2];
+        let destination_id = &hex[2..4];
+        let weight = &hex[4..20];
+
+        let source_id = u8::from_str_radix(source_id, 16)?;
+        let destination_id = u8::from_str_radix(destination_id, 16)?;
+        let weight = f64::from_bits(u64::from_str_radix(weight, 16)?);
+
+        Ok(Self {
+            source_id,
+            destination_id,
+            weight,
+        })
+    }
+
+    pub fn as_hex(&self) -> FStr<20> {
+        let hex = format!(
+            "{:02x}{:02x}{:016x}",
+            self.source_id,
+            self.destination_id,
+            self.weight.to_bits(),
+        );
+
+        FStr::from_str(&hex).unwrap()
+    }
+}
