1b17a562c3d5e5f6757b587ea85cb900eec10b67
Sam Cutter <swcutter@pm.me>
2024-10-06 18:18:16 +0100

    Created a function which converts a hex string to a Gene

--- src/creature/genome/mod.rs
+++ src/creature/genome/mod.rs
@@ -1,8 +1,28 @@
 pub struct Genome(Vec<Gene>);
 
-// TODO: Implement a function which converts a hex string to a Gene.
+use fstr::FStr;
+use std::num::ParseIntError;
+
 pub struct Gene {
-    source_id: u8,
-    destination_id: u8,
-    weight: f64,
+    pub source_id: u8,
+    pub destination_id: u8,
+    pub weight: f64,
+}
+
+impl Gene {
+    pub fn from_hex(hex: FStr<20>) -> Result<Self, ParseIntError> {
+        let source_id = &hex[0..2];
+        let destination_id = &hex[2..4];
+        let weight = &hex[4..20];
+
+        let source_id = u8::from_str_radix(source_id, 16)?;
+        let destination_id = u8::from_str_radix(destination_id, 16)?;
+        let weight = f64::from_bits(u64::from_str_radix(weight, 16)?);
+
+        Ok(Self {
+            source_id,
+            destination_id,
+            weight,
+        })
+    }
 }
