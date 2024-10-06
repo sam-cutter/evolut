94177480f53d3d5cd56cbabfff77448354506e41
Sam Cutter <swcutter@pm.me>
2024-10-06 23:12:11 +0100

    Updated neuron module to reflect new changes

--- src/creature/brain/neuron/mod.rs
+++ src/creature/brain/neuron/mod.rs
@@ -1,9 +1,13 @@
 mod action;
 mod internal;
+mod neurons;
 mod sensory;
 
 pub use action::ActionNeuron;
 pub use internal::InternalNeuron;
+pub use neurons::Neurons;
+// TODO: figure out how LineOfSight should be accessed
+pub use sensory::LineOfSight;
 pub use sensory::SensoryNeuron;
 
 pub enum Neuron {
