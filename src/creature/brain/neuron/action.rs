2f294c9bfbf53f0811088b0edba39bba96548414
Sam Cutter <swcutter@pm.me>
2024-10-14 18:39:53 +0100

    Created a constructor for ActionNeuron.

--- src/creature/brain/neuron/action.rs
+++ src/creature/brain/neuron/action.rs
@@ -5,6 +5,18 @@ pub struct ActionNeuron {
     output: ActionOutput,
 }
 
+impl ActionNeuron {
+    pub fn new(action_neuron_id: u8, inputs: Vec<Connection>) -> Self {
+        let output = match action_neuron_id {
+            0 => ActionOutput::Acceleration,
+            1 => ActionOutput::AngularAcceleration,
+            _ => unreachable!(),
+        };
+
+        Self { inputs, output }
+    }
+}
+
 pub enum ActionOutput {
     Acceleration,
     AngularAcceleration,
