598120d1ab76ecafec46406549832768a003cc3e
Sam Cutter <swcutter@pm.me>
2024-10-14 18:40:10 +0100

    Created a constructor for InternalNeuron.

--- src/creature/brain/neuron/internal.rs
+++ src/creature/brain/neuron/internal.rs
@@ -3,3 +3,9 @@ use super::super::connection::Connection;
 pub struct InternalNeuron {
     inputs: Vec<Connection>,
 }
+
+impl InternalNeuron {
+    pub fn new(inputs: Vec<Connection>) -> Self {
+        Self { inputs }
+    }
+}
