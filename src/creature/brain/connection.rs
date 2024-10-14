fe3883c290d05667464cd05f169abc9664c8697a
Sam Cutter <swcutter@pm.me>
2024-10-14 18:38:51 +0100

    Implemented a constructor for Connection.

--- src/creature/brain/connection.rs
+++ src/creature/brain/connection.rs
@@ -7,6 +7,12 @@ pub struct Connection {
     weight: f64,
 }
 
+impl Connection {
+    pub fn new(input: InputNeuron, weight: f64) -> Self {
+        Self { input, weight }
+    }
+}
+
 pub enum InputNeuron {
     Sensory(Rc<SensoryNeuron>),
     Internal(Rc<InternalNeuron>),
