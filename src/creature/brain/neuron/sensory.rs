48f06837811dc643b94fe10dc77a6446de7c5385
Sam Cutter <swcutter@pm.me>
2024-10-14 18:39:36 +0100

    Updated constructor for SensoryNeuron to return Self.

--- src/creature/brain/neuron/sensory.rs
+++ src/creature/brain/neuron/sensory.rs
@@ -1,34 +1,30 @@
-use std::rc::Rc;
-
-use crate::creature::brain::connection::InputNeuron;
-
 pub struct SensoryNeuron {
     input: SensoryInput,
 }
 
 impl SensoryNeuron {
-    pub fn new(sensory_neuron_id: u8) -> InputNeuron {
-        InputNeuron::Sensory(Rc::new(Self {
-            input: match sensory_neuron_id {
-                0 => SensoryInput::Age,
-                1 => SensoryInput::Speed,
-                2 => SensoryInput::AngularVelocity,
-                3..=11 => SensoryInput::LineOfSight(match sensory_neuron_id {
-                    3 => LineOfSight::LeftCreature,
-                    4 => LineOfSight::LeftFood,
-                    5 => LineOfSight::LeftObstacle,
-                    6 => LineOfSight::MiddleCreature,
-                    7 => LineOfSight::MiddleFood,
-                    8 => LineOfSight::MiddleObstacle,
-                    9 => LineOfSight::RightCreature,
-                    10 => LineOfSight::RightFood,
-                    11 => LineOfSight::RightObstacle,
-                    _ => unreachable!(),
-                }),
-                12 => SensoryInput::StoredEnergy,
+    pub fn new(sensory_neuron_id: u8) -> Self {
+        let input = match sensory_neuron_id {
+            0 => SensoryInput::Age,
+            1 => SensoryInput::Speed,
+            2 => SensoryInput::AngularVelocity,
+            3..=11 => SensoryInput::LineOfSight(match sensory_neuron_id {
+                3 => LineOfSight::LeftCreature,
+                4 => LineOfSight::LeftFood,
+                5 => LineOfSight::LeftObstacle,
+                6 => LineOfSight::MiddleCreature,
+                7 => LineOfSight::MiddleFood,
+                8 => LineOfSight::MiddleObstacle,
+                9 => LineOfSight::RightCreature,
+                10 => LineOfSight::RightFood,
+                11 => LineOfSight::RightObstacle,
                 _ => unreachable!(),
-            },
-        }))
+            }),
+            12 => SensoryInput::StoredEnergy,
+            _ => unreachable!(),
+        };
+
+        Self { input }
     }
 }
 
