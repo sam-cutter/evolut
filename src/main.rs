fdf19b80ffb874ebbd370f80411303af3ccb5781
Sam Cutter <swcutter@pm.me>
2024-10-14 22:03:38 +0100

    Created a small test for the Brain constructor.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use evolut::creature::{brain::Brain, genome::Gene};
+
 fn main() {
-    println!("Hello, world!");
+    let genes = vec![
+        Gene::new(0, 0, 0.1),
+        Gene::new(0, 128, 0.2),
+        Gene::new(129, 130, 0.3),
+        Gene::new(131, 130, 0.4),
+        Gene::new(130, 1, 0.5),
+        Gene::new(1, 131, 0.6),
+        Gene::new(2, 131, 0.7),
+        Gene::new(3, 132, 0.8),
+        Gene::new(132, 131, 0.9),
+        Gene::new(130, 132, 1.0),
+        Gene::new(131, 131, 1.1),
+    ];
+
+    let brain = Brain::new(&genes);
+
+    for neuron in brain.neurons() {
+        println!("{:#?}", neuron);
+    }
 }
