d283ed3be0de38482a42f5af57fa71b67ef5dba7
Sam Cutter <swcutter@pm.me>
2024-10-16 19:51:48 +0100

    Continued documenting the creature module.

--- src/creature/mod.rs
+++ src/creature/mod.rs
@@ -1,4 +1,7 @@
 //! Houses all code relating to creatures.
+//!
+//! A creature lives in the simulation world, and acts according to the action output neurons within its [brain::Brain]. A new creature
+//! is built according to its [genome::Genome].
 
 pub mod brain;
 pub mod genome;
