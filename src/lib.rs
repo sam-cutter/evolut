95285e9566372eebe2a5b443a2b9d7081f54f379
Sam Cutter <swcutter@pm.me>
2024-10-16 19:38:41 +0100

    Documented crate front page.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
+//! Evolut is a simulation of the evolution of biological neural networks, built in Rust.
+//!
+//! It is heavily inspired by David R. Miller's [biosim4](https://github.com/davidrmiller/biosim4), which is more commonly known by
+//! its incredible accompanying video ["I programmed some creatures. They evolved."](https://youtu.be/N3tRFayqVtk)
+
 pub mod creature;
 pub mod simulation;
