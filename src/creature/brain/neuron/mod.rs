ccfe1a034103c76c898025b45158c86f0b2101d2
Sam Cutter <swcutter@pm.me>
2024-11-13 20:20:32 +0000

    Created a trait which facilitates accessing the activation of a neuron.

--- src/creature/brain/neuron/mod.rs
+++ src/creature/brain/neuron/mod.rs
@@ -2,9 +2,10 @@ mod action;
 mod internal;
 mod sensory;
 
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 pub use action::ActionNeuron;
+pub use action::ActionOutput;
 pub use internal::InternalNeuron;
 pub use sensory::SensoryNeuron;
 
@@ -14,4 +15,8 @@ pub enum Neuron {
     Sensory(Arc<SensoryNeuron>),
     Internal(Arc<InternalNeuron>),
     Action(Arc<ActionNeuron>),
+}
+
+pub trait Activation {
+    fn activation(&self, internal_activation_cache: &mut HashMap<Arc<InternalNeuron>, f64>) -> f64;
 }
