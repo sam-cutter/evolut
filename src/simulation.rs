b230a1a11aea128e4efb0639c25c4f8eb7434ee5
Sam Cutter <swcutter@pm.me>
2025-01-04 14:37:45 +0000

    Fixed a small typo.

--- src/simulation.rs
+++ src/simulation.rs
@@ -4,5 +4,5 @@
 pub const MAX_INTERNAL_NEURONS: u8 = 5;
 /// The maximum number of genes that may exist in a creature's genome.
 pub const GENOME_LENGTH: usize = 10;
-/// The number of creatures in the first
+/// The number of creatures in the first generation.
 pub const GENERATION_ZERO_SIZE: u32 = 1000;
